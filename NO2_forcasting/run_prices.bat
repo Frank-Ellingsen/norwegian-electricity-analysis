647d95c9df830dcddcdf2ec2c64c81518dd6f611
FrankE <frankellingsen@hotmail.com>
2026-05-08 21:10:09 +0200

    update

--- NO2_forcasting/run_prices.bat
+++ NO2_forcasting/run_prices.bat
@@ -4,6 +4,4 @@ REM Go to project root
 cd /d "C:\Users\Lenovo\OneDrive\Desktop\datafrank\norwegian-electricity-analysis\NO2_forcasting"
 
 REM Run script from pipeline folder
-"C:\Users\Lenovo\miniconda3\envs\no2_forecasting\python.exe" ^
-pipeline\daily_hourly_no_prices_to_db.py ^
->> output.log 2>&1
+"C:\Users\Lenovo\miniconda3\envs\no2_forecasting\python.exe" pipeline\daily_hourly_no_prices_to_db.py >> output.log 2>&1
--- NO2_forcasting/run_timeseries.bat
+++ NO2_forcasting/run_timeseries.bat
@@ -4,6 +4,4 @@ REM Go to project root
 cd /d "C:\Users\Lenovo\OneDrive\Desktop\datafrank\norwegian-electricity-analysis\NO2_forcasting"
 
 REM Run timeseries pipeline
-"C:\Users\Lenovo\miniconda3\envs\no2_forecasting\python.exe" ^
-pipeline\no2_timeseries_pipeline.py ^
->> output.log 2>&1
+"C:\Users\Lenovo\miniconda3\envs\no2_forecasting\python.exe" pipeline\no2_timeseries_pipeline.py >> output.log 2>&1
