75a74de241e97fef35659cb6551d877be9db2e4f
FrankE <frankellingsen@hotmail.com>
2026-05-09 12:58:25 +0200

    update

--- NO2_forcasting/run_timeseries.bat
+++
@@ -1,7 +0,0 @@
-@echo on
-
-cd /d "C:\Users\Lenovo\OneDrive\Desktop\datafrank\norwegian-electricity-analysis\NO2_forcasting"
-
-"C:\Users\Lenovo\miniconda3\envs\no2_forecasting\python.exe" "pipeline\no2_timeseries_pipeline.py" >> output.log 2>&1
-
-pause
