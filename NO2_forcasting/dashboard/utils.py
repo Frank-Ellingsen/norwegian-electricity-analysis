34ab5736d2c3b23e72f9952a63f101abb223ee6c
FrankE <frankellingsen@hotmail.com>
2026-05-11 19:30:54 +0200

    update

--- NO2_forcasting/dashboard/utils.py
+++
@@ -1,2 +0,0 @@
-# This file can be used for utility functions if needed.
-# For now, it remains empty as per the instructions.
--- NO2_forcasting/data/sqlite/run_merge.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cd /d C:\path\to\db
-sqlite3 database.db < merge_prices.sql
